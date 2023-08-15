10243ed7852d94175ad43d4cf98147ea82707111
Human <reedwith2es@gmail.com>
2023-08-14 22:29:33 -0700

    Broken: Build works but dev server doesn't recompile

--- src/all.mjs
+++ src/all.mjs
@@ -1,9 +1,24 @@
-import fsp from 'node:fs/promises';
+import { readdir, stat } from 'node:fs/promises';
 
 const POST_DIR_REL_PATH = '../posts'
+const OUT_DIR_REL_PATH = '../build'
 
-export default async ({ file, optionsString }) =>
-{
+const exists = async (path) => {
+    try {
+        await stat(path)
+    } catch (error) {
+        return false;
+    }
+
+    return true;
+}
+
+export default async () => {
+    // First, check if the output directory exists
+    const outputDirectoryExists = exists(OUT_DIR_REL_PATH)
+
+    if (!outputDirectoryExists) console.warn(`Rebuilding entire non-existant output directory \`${OUT_DIR_REL_PATH}\``)
+
     //
     // Create and default export an array of MDX imports
     //
@@ -12,20 +27,43 @@ export default async ({ file, optionsString }) =>
     let js = '';
     let nextImportIndex = 0;
 
-    for (const file of await fsp.readdir(POST_DIR_REL_PATH))
-    {
-        if (!file.endsWith('.mdx'))
+    for (const inputFileName of await readdir(POST_DIR_REL_PATH)) {
+        if (!inputFileName.endsWith('.mdx'))
             continue;
+
+        const postPath = `${POST_DIR_REL_PATH}/${inputFileName}`;
+        const fileNameWithoutExtension = inputFileName.replace(/\.mdx$/, '');
+        const outputFileName = `${fileNameWithoutExtension}.html`
+        let shouldCompile = true;
+
+        // We'll skip this file if the output is newer than the input
+        if (outputDirectoryExists) {
+            // Get the most recent time the input file was modified
+            const { mtimeMs: inputModified } = await stat(postPath)
+
+            // Also get the most recent time the output was modified
+            const { mtimeMs: outputModified } = await stat(`${OUT_DIR_REL_PATH}/${outputFileName}`)
+
+            if (outputModified > inputModified) {
+                console.warn(`Skipping ${fileNameWithoutExtension} as the output is newer`)
+                // NOTE
+                // In the future, I'd like to just `continue` here and not even
+                // export it. But if it's not imported right now there's a bug in the
+                // development server where it will never register it as a file to watch.
+                // So instead right now we just pass a flag to not compile it
+                shouldCompile = false;
+            };
+        }
 
         // Import the MDX file via the :mdx: plugin
         const identifier = `mdx_${nextImportIndex++}`
-        js += `import ${identifier} from ':mdx:${POST_DIR_REL_PATH}/${file}';\n`;
+        js += `import ${identifier} from ':mdx:${postPath}';\n`;
 
         // Add the imported result to an array along with some meta information
-        mdx.push(`[${JSON.stringify({ file })}, ${identifier}]`);
+        mdx.push(`[${JSON.stringify({ inputFileName, outputFileName, shouldCompile })}, ${identifier}]`);
     }
 
     const result = `${js}export default [${mdx.join(",")}];`;
-    console.log('Result of all.js:', result)
+    console.log('Result of all.mjs:\n', result)
     return result;
 }
---
+++ src/compile-all-page.jsx
@@ -0,0 +1,21 @@
+import { Page } from '@nakedjsx/core/page';
+import posts from ':dynamic:./all.mjs'
+
+for (const [{ inputFileName, outputFileName, shouldCompile }, Post] of posts) {
+    // See NOTE in `all.mjs` Line ~50. This should go away when the flag goes away.
+    if (!shouldCompile) {
+        console.log(`not compiling ${outputFileName} as requested`)
+        continue;
+    } else {
+        console.log(`compiling ${outputFileName}!`)
+    }
+    Page.Create('en');
+    Page.AppendBody(
+        <main>
+            <h1>Body Content H1</h1>
+            <Post originFilename={inputFileName} />
+        </main>
+    );
+
+    Page.Render(outputFileName);
+}
