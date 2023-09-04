3537d45c6919b4fb384bbaeb243290ab0603fb40
Reed Spool <reedwith2es@gmail.com>
2023-09-04 08:49:55 -0700

    Add utility components to page compile script

--- src/compile-one-off-mdx-page.jsx
+++ src/compile-one-off-mdx-page.jsx
@@ -2,13 +2,15 @@ import { Page } from '@nakedjsx/core/page';
 import { OUT_DIR_REL_PATH } from './constants.mjs'
 import Content from ':mdx:../one-off-mdx-pages/project-add-popout.mdx';
 import { ScriptTagExperiment } from "./script-tag-component-test.jsx"
+import { Link } from "../components/Link.jsx"
+import { Future } from "../components/Future.jsx"
 
 Page.Create('en');
 Page.AppendHead(<title>Reed's Website</title>)
 Page.AppendHead(<link rel="stylesheet" href="./build.css" />)
 Page.AppendBody(
     <main class="cpnt-blog-article">
-        <Content {...{ ScriptTagExperiment }} />
+        <Content {...{ ScriptTagExperiment }} components={{ Link, Future }} />
         <ScriptTagExperiment />
     </main>
 );
