a6c5f52c4b11966660f92c85a1929d07df2910fd
Human <reedwith2es@gmail.com>
2023-09-04 18:18:02 -0700

    Replace dynamic post import collection with on-demand script

--- src/compile-all-posts-page.jsx
+++ src/compile-all-posts-page.jsx
@@ -1,17 +1,10 @@
 import { Page } from '@nakedjsx/core/page';
-import posts from ':dynamic:./all.mjs'
+import posts from './generated-post-imports.mjs'
 import { Link } from "../components/Link.jsx"
 import { Future } from "../components/Future.jsx"
 
 
-for (const [{ inputFileName, outputFileName, shouldCompile }, Post] of posts) {
-    // See NOTE in `all.mjs` Line ~50. This should go away when the flag goes away.
-    if (!shouldCompile) {
-        console.log(`not compiling ${outputFileName} as requested`)
-        continue;
-    } else {
-        console.log(`compiling ${outputFileName}!`)
-    }
+for (const [{ inputFileName, outputFileName }, Post] of posts) {
     Page.Create('en');
     Page.AppendHead(<title>Reed's Website</title>)
     Page.AppendHead(<meta name="viewport" content="width=device-width, initial-scale=1" />)
