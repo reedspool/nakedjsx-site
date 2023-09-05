2b3623870205958c07bdd4a907511981a10cd56f
Human <reedwith2es@gmail.com>
2023-09-04 17:22:17 -0700

    Future components as paragraphs, with the option to switch

---
+++ components/Elements.jsx
@@ -0,0 +1,2 @@
+export const P = ({ children, ...props }) => <p {...props}>{children}</p>
+export const Span = ({ children, ...props }) => <span {...props}>{children}</span>
--- components/Future.jsx
+++ components/Future.jsx
@@ -1,3 +1,5 @@
-export const Future = ({ children }) => {
-    return <span>Future: <span>{children}</span></span>
+import { P } from "./Elements.jsx"
+
+export const Future = ({ children, Wrap = P }) => {
+    return <Wrap>Future: <span>{children}</span></Wrap>
 }
