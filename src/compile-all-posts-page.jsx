52e4397755dc3c827521d79244dfc739f60e005c
Human <reedwith2es@gmail.com>
2023-09-07 16:58:17 -0700

    Use link instead of @import for fonts (which were broken)

--- src/compile-all-posts-page.jsx
+++ src/compile-all-posts-page.jsx
@@ -8,10 +8,15 @@ for (const [{ inputFileName, outputFileName }, Post] of posts) {
     Page.Create('en');
     Page.AppendHead(<title>Reed's Website</title>)
     Page.AppendHead(<meta name="viewport" content="width=device-width, initial-scale=1" />)
+    Page.AppendHead(<link rel="preconnect" href="https://fonts.googleapis.com" />)
+    Page.AppendHead(<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />)
+    Page.AppendHead(<link href="https://fonts.googleapis.com/css2?family=Jost&family=Karla&display=swap" rel="stylesheet" />)
     Page.AppendHead(<link rel="stylesheet" href="./build.css" />)
     Page.AppendBody(
         <>
-            <header class="sticky"><Link slug="home">Home</Link></header>
+            <header class="sticky">
+                    <Link slug="home">Reed's Website</Link>
+            </header>
             <main class="cpnt-blog-article">
                 <Post originFilename={inputFileName} components={{ Link, Future }} />
             </main>
