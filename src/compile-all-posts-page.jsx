f679c74caa21a5217c3970cb5e45300b2eca3694
Human <reedwith2es@gmail.com>
2023-09-07 21:13:03 -0700

    Fix footer always on bottom whether short or long content

--- src/compile-all-posts-page.jsx
+++ src/compile-all-posts-page.jsx
@@ -14,12 +14,24 @@ for (const [{ inputFileName, outputFileName }, Post] of posts) {
     Page.AppendHead(<link rel="stylesheet" href="./build.css" />)
     Page.AppendBody(
         <>
-            <header class="sticky">
-                    <Link slug="home">Reed's Website</Link>
-            </header>
-            <main class="cpnt-blog-article">
-                <Post originFilename={inputFileName} components={{ Link, Future }} />
-            </main>
+            {/*
+                Wrap the header and the header AND main content in a growing
+                container so that A) the sticky header never scrolls off the
+                page (See https://stackoverflow.com/a/47352847) and B) the footer
+                always remains squarely on the bottom of the page whether the
+                content is shorter than the screen size or way longer. Depends on
+                html and body having `height: 100%;` and body being flex-column.
+              */}
+            <div class="flex-grow">
+                <header class="sticky">
+                    <div class=" flex flex-row gap-4 items-center font-flashy">
+                        <Link slug="home">Reed's Website</Link>
+                    </div>
+                </header>
+                <main class="cpnt-blog-article">
+                    <Post originFilename={inputFileName} components={{ Link, Future }} />
+                </main>
+            </div>
             <footer><Link slug="home">Home</Link></footer>
         </>
     );
