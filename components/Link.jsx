73424fd99d713ef4e4f32ffe53800b275b4f3140
Human <reedwith2es@gmail.com>
2023-09-17 00:57:22 -0700

    Formatting

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,15 +1,15 @@
 export const slugsToHrefs = {
-    "home": "/",
-    "improve-this-website": "project-improve-this-website.html",
-    "projects-on-this-site": "project-track-my-projects.html",
-    "about-project-logs": "topic-project-logs.html",
-    "project-index": "topic-project-index.html",
-    "project-add-popout": "project-add-popout.html",
-    "project-write-script-and-style-tags": "project-write-script-and-style-tags.html",
-    "project-precision-timer": "project-precision-timer.html"
-}
+  home: "/",
+  "improve-this-website": "project-improve-this-website.html",
+  "projects-on-this-site": "project-track-my-projects.html",
+  "about-project-logs": "topic-project-logs.html",
+  "project-index": "topic-project-index.html",
+  "project-add-popout": "project-add-popout.html",
+  "project-write-script-and-style-tags":
+    "project-write-script-and-style-tags.html",
+  "project-precision-timer": "project-precision-timer.html",
 export const Link = ({ slug, children }) => {
-    const href = slugsToHrefs[slug];
-    if (!href) throw new Error(`No href found for slug "${slug}"`)
-    return <a href={href}>{children}</a>
-}
+  const href = slugsToHrefs[slug];
+  if (!href) throw new Error(`No href found for slug "${slug}"`);
+  return <a href={href}>{children}</a>;
+};
--- src/compile-all-posts-page.jsx
+++ src/compile-all-posts-page.jsx
@@ -1,24 +1,33 @@
-import { Page } from '@nakedjsx/core/page';
-import posts from './generated-post-imports.mjs'
-import { Link } from "../components/Link.jsx"
-import { Future } from "../components/Future.jsx"
-import { LogoSVG, LogoSVGSymbol } from "../components/LogoSVGSymbol.jsx"
-
-
+import { Page } from "@nakedjsx/core/page";
+import posts from "./generated-post-imports.mjs";
+import { Link } from "../components/Link.jsx";
+import { Future } from "../components/Future.jsx";
+import { LogoSVG, LogoSVGSymbol } from "../components/LogoSVGSymbol.jsx";
 
 for (const [{ inputFileName, outputFileName }, Post] of posts) {
-    Page.Create('en');
-    Page.AppendHead(<title>Reed's Website</title>)
-    Page.AppendHead(<meta name="viewport" content="width=device-width, initial-scale=1" />)
-    Page.AppendHead(<link rel="preconnect" href="https://fonts.googleapis.com" />)
-    Page.AppendHead(<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />)
-    Page.AppendHead(<link href="https://fonts.googleapis.com/css2?family=Jost&family=Karla&display=swap" rel="stylesheet" />)
-    Page.AppendHead(<link rel="stylesheet" href="./build.css" />)
+  Page.Create("en");
+  Page.AppendHead(<title>Reed's Website</title>);
+  Page.AppendHead(
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  );
+  Page.AppendHead(
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+  );
+  Page.AppendHead(
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  );
+  Page.AppendHead(
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost&family=Karla&display=swap"
+      rel="stylesheet"
+    />
+  );
+  Page.AppendHead(<link rel="stylesheet" href="./build.css" />);
 
-    Page.AppendBody(
-        <>
-            <LogoSVGSymbol />
-            {/*
+  Page.AppendBody(
+    <>
+      <LogoSVGSymbol />
+      {/*
                 Wrap the header and the header AND main content in a growing
                 container so that A) the sticky header never scrolls off the
                 page (See https://stackoverflow.com/a/47352847) and B) the footer
@@ -26,20 +35,22 @@ for (const [{ inputFileName, outputFileName }, Post] of posts) {
                 content is shorter than the screen size or way longer. Depends on
                 html and body having `height: 100%;` and body being flex-column.
               */}
-            <div class="flex-grow">
-                <header class="sticky">
-                    <div class=" flex flex-row gap-4 items-center font-flashy">
-                        <LogoSVG />
-                        <Link slug="home">Reed's Website</Link>
-                    </div>
-                </header>
-                <main class="cpnt-blog-article">
-                    <Post originFilename={inputFileName} components={{ Link, Future }} />
-                </main>
-            </div>
-            <footer><Link slug="home">Home</Link></footer>
-        </>
-    );
+      <div class="flex-grow">
+        <header class="sticky">
+          <div class=" flex flex-row gap-4 items-center font-flashy">
+            <LogoSVG />
+            <Link slug="home">Reed's Website</Link>
+          </div>
+        </header>
+        <main class="cpnt-blog-article">
+          <Post originFilename={inputFileName} components={{ Link, Future }} />
+        </main>
+      </div>
+      <footer>
+        <Link slug="home">Home</Link>
+      </footer>
+    </>
+  );
 
-    Page.Render(outputFileName);
+  Page.Render(outputFileName);
 }
