a645b7548a0e18a6491c5309dcc0fd4de09e00bc
Human <reedwith2es@gmail.com>
2023-09-20 00:21:23 -0700

    Update domain to reeds.website after configuring Netlify DNS

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,3 +1,3 @@
 export const OUT_DIR_REL_PATH = "../build";
-export const DOMAIN = "nakedjsx-reeds-website.netlify.app";
+export const DOMAIN = "reeds.website";
 export const BASE_URL = `https://${DOMAIN}`;
