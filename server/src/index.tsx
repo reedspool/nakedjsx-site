92a64817e9503f37ed311e53be67e4cc4da6da0e
Human <reedwith2es@gmail.com>
2023-12-29 15:35:24 -0800

    Logging server request timing in express

--- server/src/index.tsx
+++ server/src/index.tsx
@@ -3,10 +3,12 @@ import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 import cookieParser from "cookie-parser";
 import { Database } from "./supabaseGeneratedTypes";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(
@@ -34,15 +36,21 @@ app.get("/", async (_, res) => {
 
 type AuthBody = { refreshToken?: string; accessToken?: string };
 
+// TODO I'm reusing these clients and setting the session - is that right? Seems likely
+// that I've got a race condition where an async thing is expecting the session to still exist but it doesn't
+const supabase = createClient<Database>(
+  "https://yhuswwhmfuptgznlkdvv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodXN3d2htZnVwdGd6bmxrZHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwNzgyNjksImV4cCI6MjAxNDY1NDI2OX0.vF_fbpeSORP5ve5wVVty4lm5HaOAwGSgQxq4s39udpM",
+);
 const giveMeAnAuthenticatedSupabaseClient = async (
   req: express.Request<{}, {}, AuthBody>,
 ) => {
-  const supabase = createClient<Database>(
-    "https://yhuswwhmfuptgznlkdvv.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodXN3d2htZnVwdGd6bmxrZHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwNzgyNjksImV4cCI6MjAxNDY1NDI2OX0.vF_fbpeSORP5ve5wVVty4lm5HaOAwGSgQxq4s39udpM",
-  );
-
-
+  const startTime = process.hrtime();
+  {
+    const [seconds, nanos] = process.hrtime(startTime);
+    const durationInMillis = (seconds * 1000000000 + nanos) / 1000000; // convert first to ns then to ms
+    console.log(`Start: ${durationInMillis}`);
+  }
   let refreshToken = req.query.refreshToken;
   let accessToken = req.query.accessToken;
   if (req.method === "POST") {
@@ -63,10 +71,20 @@ const giveMeAnAuthenticatedSupabaseClient = async (
     throw new Error("User is not authenticated.");
   }
 
+  {
+    const [seconds, nanos] = process.hrtime(startTime);
+    const durationInMillis = (seconds * 1000000000 + nanos) / 1000000; // convert first to ns then to ms
+    console.log(`Before set session: ${durationInMillis}`);
+  }
   await supabase.auth.setSession({
     refresh_token: refreshToken,
     access_token: accessToken,
   });
+  {
+    const [seconds, nanos] = process.hrtime(startTime);
+    const durationInMillis = (seconds * 1000000000 + nanos) / 1000000; // convert first to ns then to ms
+    console.log(`after set session: ${durationInMillis}`);
+  }
 
   const { data: _, error: authError } = await supabase.auth.getUser();
 
@@ -75,6 +93,11 @@ const giveMeAnAuthenticatedSupabaseClient = async (
     throw new Error("Problem authenticating");
   }
 
+  {
+    const [seconds, nanos] = process.hrtime(startTime);
+    const durationInMillis = (seconds * 1000000000 + nanos) / 1000000; // convert first to ns then to ms
+    console.log(`returning client: ${durationInMillis}`);
+  }
   return supabase;
 };
 
