9bb3421182695843581113235b2e30cccebcc6d0
Human <reedwith2es@gmail.com>
2024-05-20 12:25:46 -0700

    Counting game progress, printing cards with backs

--- pages/counting-up-party-game.tsx
+++ pages/counting-up-party-game.tsx
@@ -183,178 +183,13 @@ export const MaterialSymbol = ({
     {symbolsToMaterialSymbolsContent[which]}
   </span>
 );
-export const Body = () => (
-  <div class="container">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    />
-    <style>{css`
-      .material-symbols-outlined {
-        font-variation-settings:
-          "FILL" 0,
-          "wght" 400,
-          "GRAD" 0,
-          "opsz" 24;
-        font-size: inherit;
-      }
-      body {
-        --fancy-font-family: "Special Elite", system-ui;
-
-        font-size: 20pt;
-        line-height: 1.2em;
-        --font-color: #3e3e3e;
-        color: var(--font-color);
-      }
-      @page {
-        margin: 0.1in;
-        size: 8.5in 11in;
-        @top-right {
-          content: "Page " counter(pageNumber);
-          border: 5px solid red;
-        }
-      }
-      .container {
-        /* https://en.wikipedia.org/wiki/Bicycle_Playing_Cards */
-        --poker-card-height: calc(3.5in - (1in / 16));
-        --poker-card-width: calc(2.5in - (1in / 32));
-        /* https://themagiccafe.com/forums/viewtopic.php?topic=382099 */
-        --poker-card-corner-radius: 0.125in;
-        --card-height: var(--poker-card-height);
-        --card-width: var(--poker-card-width);
-        /* empirical, want the inner corner to appear at the center of the curve */
-        --card-padding-y: 0.15in;
-        --card-padding-x: 0.12in;
-        --card-corner-radius: var(--poker-card-corner-radius);
-        --margin: 0.05in;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        align-items: center;
-        gap: var(--margin);
-      }
-
-      .container--grid {
-        /* Preserving the grid try while I switch to flex */
-
-        display: grid;
-        grid-gap: 0.05in;
-        grid-template-columns: repeat(auto-fill, var(--card-width));
-        grid-template-rows: repeat(auto-fill, var(--card-height));
-        grid-template-rows: auto;
-        justify-content: space-evenly;
-        align-content: space-evenly;
-        justify-items: space-evenly;
-        align-items: space-evenly;
-      }
-      .card__container {
-        box-sizing: border-box;
-        height: var(--card-height);
-        width: var(--card-width);
-        border-radius: var(--card-corner-radius);
-        background-color: darkgray;
-        padding: var(--card-padding-y) var(--card-padding-x);
-      }
-
-      .card__container:nth-of-type(9n) {
-        break-after: page;
-      }
-
-      .card {
-        box-sizing: border-box;
-        font-family: var(--fancy-font-family);
-        height: calc(var(--card-height) - (2 * var(--card-padding-y)));
-        width: calc(var(--card-width) - (2 * var(--card-padding-x)));
-        background-color: #e3e3e3;
-        padding: 0.1in; /* empirical */
-
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-      }
-      .card.card-number {
-        font-family: var(--fancy-font-family);
-        font-size: 72pt;
-      }
-
-      .card-header {
-        font-size: 24pt;
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-      }
-      .card-header--bottom {
-        transform-origin: center center;
-        transform: rotate(180deg);
-      }
-      .card-header-tags {
-        color: darkgray;
-        font-size: 16pt;
-      }
-      .card.card-number[data-number="6"] .card-header,
-      .card.card-number[data-number="9"] .card-header {
-        text-decoration: underline;
-      }
-
-      .card-communication .card__body {
-        padding-inline: calc(1in / 8);
-      }
-
-      .card.card-number .card__body {
-        color: darkgray;
-      }
-
-      .card-key .card__body {
-        font-size: 12pt;
-      }
-      .card-key .card__body .material-symbols-outlined {
-        vertical-align: text-bottom;
-      }
-      .card-rules .card__body {
-        font-size: 12pt;
-      }
-      .card-rules .card__body p {
-        margin: 0;
-      }
-      .page-break {
-        height: 0px;
-        width: 0px;
-        break-after: page;
-      }
-    `}</style>
-    {arrayFrom1ToN(NUM_OF_NUM_CARDS).map((n) => (
-      <NumberCard num={n} />
-    ))}
-    <SaboteurCard />
-    <KeyCard />
-    {/* <RulesCard1 /> */}
-    {/* <RulesCard2 /> */}
-    {communicationMethods.map(CommunicationMethodCard)}
-    {/* Blanks, N to fill out the current page + an entire page which one can
-      skip when they hit the print button */}
-    {arrayFrom1ToN(
-      9 -
-        ((NUM_OF_NUM_CARDS +
-          communicationMethods.length +
-          2) /* 1 saboteur, 1 key card, rules card(s) */ %
-          9) +
-        9,
-    ).map(() => (
-      <CommunicationMethodCard main={""} />
-    ))}
-  </div>
-);
-const CardContainer = ({ children }: { children: JSX.Children }) => (
-  <div class="card__container">{children}</div>
-);
+const CardContainer = ({
+  children,
+  classList = "",
+}: {
+  children: JSX.Children;
+  classList?: string;
+}) => <div class={`card__container ${classList}`}>{children}</div>;
 const Empty = () => <></>;
 const StandardCard = ({
   HeaderContent = Empty,
@@ -362,8 +197,8 @@ const StandardCard = ({
   classList = "",
 }) => {
   return (
-    <CardContainer>
-      <div class={`card ${classList}`}>
+    <CardContainer classList={classList}>
+      <div class={`card`}>
         <div class="card-header">
           <HeaderContent />
         </div>
@@ -516,3 +351,282 @@ const RulesCard2 = () => {
     />
   );
 };
+
+const InformationCardBack = () => {
+  return (
+    <StandardCard
+      classList="card-back"
+      HeaderContent={() => (
+        <>
+          <MaterialSymbol which="information" />
+          <MaterialSymbol which="information" />
+        </>
+      )}
+      BodyContent={() => (
+        <>
+          <MaterialSymbol which="information" />
+        </>
+      )}
+    />
+  );
+};
+
+const CommunicationMethodCardBack = () => {
+  return (
+    <StandardCard
+      classList="card-back"
+      HeaderContent={() => (
+        <>
+          <MaterialSymbol which="communication" />
+          <MaterialSymbol which="communication" />
+        </>
+      )}
+      BodyContent={() => (
+        <>
+          <MaterialSymbol which="communication" />
+        </>
+      )}
+    />
+  );
+};
+
+const NumberCardBack = () => {
+  return (
+    <StandardCard
+      classList="card-back card-number-back"
+      HeaderContent={() => (
+        <>
+          <span>#</span>
+          <span>#</span>
+        </>
+      )}
+      BodyContent={() => (
+        <>
+          <span>#</span>
+        </>
+      )}
+    />
+  );
+};
+
+const CardFronts: Array<JSX.Element> = [];
+let lastCardFrontsLength = CardFronts.length;
+CardFronts.push(
+  ...arrayFrom1ToN(NUM_OF_NUM_CARDS).map((n) => <NumberCard num={n} />),
+);
+CardFronts.push(<SaboteurCard />);
+const numNumCards = CardFronts.length - lastCardFrontsLength;
+lastCardFrontsLength = CardFronts.length;
+CardFronts.push(...communicationMethods.map(CommunicationMethodCard));
+const numCommunicationCards = CardFronts.length - lastCardFrontsLength;
+lastCardFrontsLength = CardFronts.length;
+/* Blanks, N to fill out the current page + an entire page which one can
+      skip when they hit the print button */
+CardFronts.push(<KeyCard />);
+/*CardFronts.push(   <RulesCard1 />, )*/
+/*CardFronts.push( <RulesCard2 />, ) */
+const numInfoCards = CardFronts.length - lastCardFrontsLength;
+lastCardFrontsLength = CardFronts.length;
+
+const numNonblankCards = CardFronts.length;
+
+// Add a number of blank cards to
+//   1. Fill up the rest of the last sheet of non-blank cards (9 - (N % 9))
+//   2. Fill up another sheet of all blank cards (+ 9)
+const numBlankCards = 9 - (numNonblankCards % 9) + 9;
+CardFronts.push(
+  ...Array(numBlankCards).fill(<CommunicationMethodCard main={""} />),
+);
+
+const CardBacks: Array<JSX.Element> = [];
+CardBacks.push(...Array(numNumCards).fill(<NumberCardBack />));
+CardBacks.push(
+  ...Array(numCommunicationCards).fill(<CommunicationMethodCardBack />),
+);
+CardBacks.push(...Array(numInfoCards).fill(<InformationCardBack />));
+CardBacks.push(...Array(numBlankCards).fill(<CommunicationMethodCardBack />));
+if (CardBacks.length !== CardFronts.length)
+  throw new Error(
+    `# of card fronts (${CardFronts.length}) did not match # backs (${CardBacks.length})`,
+  );
+
+// Now intersperse card backs. After every 9 card fronts, insert 9 card backs.
+const CardFrontsAndBacks: Array<JSX.Element> = [];
+for (var i = 0; i < CardFronts.length; i += 9) {
+  for (var j = i; j < i + 9; j++) {
+    const card = CardFronts[j];
+    if (!card) throw new Error("CardFronts underflowed");
+    CardFrontsAndBacks.push(card);
+  }
+  for (var j = i; j < i + 9; j++) {
+    const card = CardBacks[j];
+    if (!card) throw new Error("CardBacks underflowed");
+    CardFrontsAndBacks.push(card);
+  }
+}
+
+if (CardFrontsAndBacks.length !== 2 * CardFronts.length)
+  throw new Error("CardFrontsAndBacks length mismatch");
+
+export const Body = () => (
+  <div class="container">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+    <style>{css`
+      .material-symbols-outlined {
+        font-variation-settings:
+          "FILL" 0,
+          "wght" 400,
+          "GRAD" 0,
+          "opsz" 24;
+        font-size: inherit;
+      }
+      body {
+        --fancy-font-family: "Special Elite", system-ui;
+
+        font-size: 20pt;
+        line-height: 1.2em;
+        --font-color: #3e3e3e;
+        color: var(--font-color);
+      }
+      @page {
+        margin: 0.1in;
+        size: 8.5in 11in;
+        @top-right {
+          content: "Page " counter(pageNumber);
+          border: 5px solid red;
+        }
+      }
+      .container {
+        /* https://en.wikipedia.org/wiki/Bicycle_Playing_Cards */
+        --poker-card-height: calc(3.5in - (1in / 16));
+        --poker-card-width: calc(2.5in - (1in / 32));
+        /* https://themagiccafe.com/forums/viewtopic.php?topic=382099 */
+        --poker-card-corner-radius: 0.125in;
+        --card-height: var(--poker-card-height);
+        --card-width: var(--poker-card-width);
+        /* empirical, want the inner corner to appear at the center of the curve */
+        --card-padding-y: 0.15in;
+        --card-padding-x: 0.12in;
+        --card-corner-radius: var(--poker-card-corner-radius);
+        --margin: 0.05in;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        gap: var(--margin);
+      }
+
+      .container--grid {
+        /* Preserving the grid try while I switch to flex */
+
+        display: grid;
+        grid-gap: 0.05in;
+        grid-template-columns: repeat(auto-fill, var(--card-width));
+        grid-template-rows: repeat(auto-fill, var(--card-height));
+        grid-template-rows: auto;
+        justify-content: space-evenly;
+        align-content: space-evenly;
+        justify-items: space-evenly;
+        align-items: space-evenly;
+      }
+      .card__container {
+        box-sizing: border-box;
+        height: var(--card-height);
+        width: var(--card-width);
+        border-radius: var(--card-corner-radius);
+        background-color: darkgray;
+        padding: var(--card-padding-y) var(--card-padding-x);
+      }
+
+      .card__container:nth-of-type(9n) {
+        break-after: page;
+      }
+
+      .card {
+        box-sizing: border-box;
+        font-family: var(--fancy-font-family);
+        height: calc(var(--card-height) - (2 * var(--card-padding-y)));
+        width: calc(var(--card-width) - (2 * var(--card-padding-x)));
+        background-color: #e3e3e3;
+        padding: 0.1in; /* empirical */
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      .card-back .card {
+        color: darkgray;
+        font-size: 24pt;
+      }
+      .card-number-back,
+      .card-number-back .card-header {
+        font-size: 28pt;
+      }
+      .card-back .card__body {
+        font-size: 72pt;
+      }
+      .card-number {
+        font-family: var(--fancy-font-family);
+        font-size: 72pt;
+      }
+
+      .card-header {
+        font-size: 24pt;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+      .card-header--bottom {
+        transform-origin: center center;
+        transform: rotate(180deg);
+      }
+      .card-header-tags {
+        color: darkgray;
+        font-size: 16pt;
+      }
+      .card-number .card[data-number="6"] .card-header,
+      .card-number .card[data-number="9"] .card-header {
+        text-decoration: underline;
+      }
+
+      .card-communication .card__body {
+        padding-inline: calc(1in / 8);
+      }
+
+      .card-number .card__body {
+        color: darkgray;
+      }
+
+      .card-key .card__body {
+        font-size: 12pt;
+      }
+      .card-key .card__body .material-symbols-outlined {
+        vertical-align: text-bottom;
+      }
+      .card-rules .card__body {
+        font-size: 12pt;
+      }
+      .card-rules .card__body p {
+        margin: 0;
+      }
+      .page-break {
+        height: 0px;
+        width: 0px;
+        break-after: page;
+      }
+    `}</style>
+    {...CardFrontsAndBacks}
+  </div>
+);
