38882b434552beb6d9011fe3aff726615e873887
Human <reedwith2es@gmail.com>
2024-09-09 16:27:35 -0700

    Fun red and blue automatic drawing

--- static/animation/client.ts
+++ static/animation/client.ts
@@ -1,6 +1,30 @@
-const { PI, cos, sin, sqrt, atan2 } = Math;
-const canvas = document.querySelector("canvas")!;
+const { PI, cos, sin, sqrt, atan2, abs } = Math;
+const TAO = PI * 2;
+const PHI = 1.618033988749895;
+const lerp = (v0: number, v1: number, t: number) =>
+    // a * t + (b - a) * t
+    v0 + t * (v1 - v0);
+const normalizeAngle = (angle: number) => (angle + TAO) % TAO;
 
+// The unit circle starts pointing directly east, and sweeps south then west
+// because Y grows downwards.
+const mapDirections = (angle: number) => {
+    angle = normalizeAngle(angle);
+    if (
+        (angle >= (15 * TAO) / 16 && angle <= TAO) ||
+        (angle >= 0 && angle <= TAO / 16)
+    )
+        return "east";
+    if (angle >= TAO / 16 && angle <= (3 * TAO) / 16) return "southeast";
+    if (angle >= (3 * TAO) / 16 && angle <= (5 * TAO) / 16) return "south";
+    if (angle >= (5 * TAO) / 16 && angle <= (7 * TAO) / 16) return "southwest";
+    if (angle >= (7 * TAO) / 16 && angle <= (9 * TAO) / 16) return "west";
+    if (angle >= (9 * TAO) / 16 && angle <= (11 * TAO) / 16) return "northwest";
+    if (angle >= (11 * TAO) / 16 && angle <= (13 * TAO) / 16) return "north";
+    if (angle >= (13 * TAO) / 16 && angle <= (15 * TAO) / 16)
+        return "northeast";
+};
+const canvas = document.querySelector("canvas")!;
 const c = canvas.getContext("2d")!; // Context
 let d: {
     // Dimensions
@@ -30,17 +54,20 @@ type Point = { x: number; y: number };
 type Circle = Point & { radius: number };
 function circle({ x, y, radius }: Circle) {
     c.beginPath();
-    c.arc(x, y, radius, 0, PI * 2, true);
-    c.stroke();
+    c.arc(x, y, radius, 0, TAO, true);
+    c.fill();
 }
 
 let numSegments: number;
 let segments: Array<Circle>;
 let mouseX: number;
 let mouseY: number;
-let drawing = false;
+let controllingWithMouse = false;
+let head: Circle & { direction: number };
 function setup() {
-    numSegments = 40;
+    c.fillStyle = "#282828"; // TODO Get calculated body font color
+    c.fillRect(0, 0, d.width, d.height);
+    numSegments = 180;
     const radius = d.width / numSegments / 1 - 1;
     segments = Array(numSegments)
         .fill(null)
@@ -49,15 +76,83 @@ function setup() {
             y: d.center.y,
             radius,
         }));
+    head = {
+        x: segments[0].x,
+        y: segments[0].y,
+        radius: radius * 4,
+        direction: 0,
+    };
 }
 function draw() {
-    c.fillStyle = "#282828"; // TODO Get calculated body font color
+    c.fillStyle = "rgba(100, 100, 100, 0.001)"; // TODO Get calculated body font color
     c.fillRect(0, 0, d.width, d.height);
     c.strokeStyle = "white"; // TODO Get calculated body font color
     c.fillStyle = "white"; // TODO Get calculated body font color
-    const head = segments[0];
-    head.x = mouseX ?? d.center.x;
-    head.y = mouseY ?? d.center.y;
+
+    // head.direction = normalizeAngle(head.direction);
+    const prevHead: Point = { ...head };
+    let nextDirection = head.direction;
+    // The head follows the mouse
+    if (controllingWithMouse) {
+        // Adjust head direction based on movement (none if no movement)
+        if (
+            abs(head.x - prevHead.x) > 0.0001 &&
+            abs(head.y - prevHead.y) > 0.0001
+        ) {
+            nextDirection = atan2(head.y - prevHead.y, head.x - prevHead.x);
+        }
+    } else {
+        const wayOut = -50;
+        const margin = -10;
+        // Way out of bounds?
+        if (
+            d.width - head.x < wayOut ||
+            d.height - head.y < wayOut ||
+            head.x < wayOut ||
+            head.y < wayOut
+        ) {
+            head.x = d.center.x;
+            head.y = d.center.y;
+            nextDirection = Math.random() * TAO;
+            head.direction = nextDirection;
+            // head.direction = 0;
+            // nextDirection = 0;
+        }
+        // normal Out of bounds?
+        else if (
+            d.width - head.x < margin ||
+            d.height - head.y < margin ||
+            head.x < margin ||
+            head.y < margin
+        ) {
+            // Turn towards the center
+            nextDirection = atan2(d.center.y - head.y, d.center.x - head.x);
+        }
+    }
+
+    head.direction =
+        lerp(
+            normalizeAngle(head.direction) + TAO,
+            normalizeAngle(nextDirection) + TAO,
+            0.1,
+        ) % TAO;
+
+    if (controllingWithMouse) {
+        head.x = mouseX ?? head.x;
+        head.y = mouseY ?? head.y;
+    } else {
+        const speed = 8;
+        // Advance
+        head.x += cos(head.direction) * speed;
+        head.y += sin(head.direction) * speed;
+    }
+
+    // The first segment follows the head
+    const first = segments[0];
+    first.x = lerp(first.x, head.x, 0.07);
+    first.y = lerp(first.y, head.y, 0.07);
+
+    // Then every segment follows the previous one
     for (let i = 1; i < segments.length; i++) {
         let last = segments[i - 1];
         let current = segments[i];
@@ -66,17 +161,36 @@ function draw() {
         const other = last.y - current.y;
         const hypoteneuse = sqrt(adjacent ** 2 + other ** 2);
         const angle = atan2(other, adjacent);
-        const moveDistance = hypoteneuse - (last.radius + current.radius);
+        const moveDistance =
+            hypoteneuse - (last.radius + current.radius) * 0.38;
 
         current.x += cos(angle) * moveDistance;
         current.y += sin(angle) * moveDistance;
     }
+    let skinPoints: Array<Point>;
+    for (let i = 1; i < segments.length; i++) {
+        let last = segments[i - 1];
+        let current = segments[i];
+    }
 
-    segments.forEach(({ x, y, radius }) => {
+    // c.fillStyle = "rgba(255, 255, 255, 0.2)";
+    // circle({ ...head });
+    // c.strokeStyle = "red";
+    // c.beginPath();
+    // c.moveTo(head.x, head.y);
+    // c.lineTo(
+    //     head.x + cos(head.direction) * PHI * head.radius,
+    //     head.y + sin(head.direction) * PHI * head.radius,
+    // );
+    // c.stroke();
+    segments.forEach(({ x, y, radius }, index) => {
+        // if (index % 2 === 1) return;
+
+        c.fillStyle = `rgba(${255 - cos((TAO * index) / numSegments) * 255}, ${
+            (cos((TAO * Date.now()) / 1000) / 2) * 255
+        }, ${cos((TAO * index) / numSegments) * 255}, 0.8)`; // TODO Get calculated body font color
         circle({ x, y, radius });
     });
-
-    if (drawing) circle({ x: mouseX, y: mouseY, radius: 5 });
 
     requestAnimationFrame(draw);
 }
@@ -91,15 +205,23 @@ canvas.addEventListener("touchmove", ({ touches }) => {
     mouseY = clientY;
 });
 canvas.addEventListener("mouseup", ({ clientX, clientY }) => {
-    drawing = false;
+    controllingWithMouse = !controllingWithMouse;
 });
 canvas.addEventListener("mousedown", ({ clientX, clientY }) => {
-    draw();
+    // draw();
+});
+
+document.body.addEventListener("keydown", (event) => {
+    const { key } = event;
+    if (key == "r") {
+        c.fillStyle = "#282828"; // TODO Get calculated body font color
+        c.fillRect(0, 0, d.width, d.height);
+    }
 });
 
 window.addEventListener("resize", () => {
     setCanvasDimensions();
-    draw();
+    // draw();
 });
 
 setCanvasDimensions();
--- static/animation/out/client.js
+++ static/animation/out/client.js
@@ -15,26 +15,61 @@ var setCanvasDimensions = function() {
 };
 var circle = function({ x, y, radius }) {
   c.beginPath();
-  c.arc(x, y, radius, 0, PI * 2, true);
-  c.stroke();
+  c.arc(x, y, radius, 0, TAO, true);
+  c.fill();
 };
 var setup = function() {
-  numSegments = 40;
+  c.fillStyle = "#282828";
+  c.fillRect(0, 0, d.width, d.height);
+  numSegments = 180;
   const radius = d.width / numSegments / 1 - 1;
   segments = Array(numSegments).fill(null).map((_, i) => ({
     x: d.width / numSegments * i + radius + 1,
     y: d.center.y,
     radius
   }));
+  head = {
+    x: segments[0].x,
+    y: segments[0].y,
+    radius: radius * 4,
+    direction: 0
+  };
 };
 var draw = function() {
-  c.fillStyle = "#282828";
+  c.fillStyle = "rgba(100, 100, 100, 0.001)";
   c.fillRect(0, 0, d.width, d.height);
   c.strokeStyle = "white";
   c.fillStyle = "white";
-  const head = segments[0];
-  head.x = mouseX ?? d.center.x;
-  head.y = mouseY ?? d.center.y;
+  const prevHead = { ...head };
+  let nextDirection = head.direction;
+  if (controllingWithMouse) {
+    if (abs(head.x - prevHead.x) > 0.0001 && abs(head.y - prevHead.y) > 0.0001) {
+      nextDirection = atan2(head.y - prevHead.y, head.x - prevHead.x);
+    }
+  } else {
+    const wayOut = -50;
+    const margin = -10;
+    if (d.width - head.x < wayOut || d.height - head.y < wayOut || head.x < wayOut || head.y < wayOut) {
+      head.x = d.center.x;
+      head.y = d.center.y;
+      nextDirection = Math.random() * TAO;
+      head.direction = nextDirection;
+    } else if (d.width - head.x < margin || d.height - head.y < margin || head.x < margin || head.y < margin) {
+      nextDirection = atan2(d.center.y - head.y, d.center.x - head.x);
+    }
+  }
+  head.direction = lerp(normalizeAngle(head.direction) + TAO, normalizeAngle(nextDirection) + TAO, 0.1) % TAO;
+  if (controllingWithMouse) {
+    head.x = mouseX ?? head.x;
+    head.y = mouseY ?? head.y;
+  } else {
+    const speed = 8;
+    head.x += cos(head.direction) * speed;
+    head.y += sin(head.direction) * speed;
+  }
+  const first = segments[0];
+  first.x = lerp(first.x, head.x, 0.07);
+  first.y = lerp(first.y, head.y, 0.07);
   for (let i = 1;i < segments.length; i++) {
     let last = segments[i - 1];
     let current = segments[i];
@@ -42,18 +77,25 @@ var draw = function() {
     const other = last.y - current.y;
     const hypoteneuse = sqrt(adjacent ** 2 + other ** 2);
     const angle = atan2(other, adjacent);
-    const moveDistance = hypoteneuse - (last.radius + current.radius);
+    const moveDistance = hypoteneuse - (last.radius + current.radius) * 0.38;
     current.x += cos(angle) * moveDistance;
     current.y += sin(angle) * moveDistance;
   }
-  segments.forEach(({ x, y, radius }) => {
+  let skinPoints;
+  for (let i = 1;i < segments.length; i++) {
+    let last = segments[i - 1];
+    let current = segments[i];
+  }
+  segments.forEach(({ x, y, radius }, index) => {
+    c.fillStyle = `rgba(${255 - cos(TAO * index / numSegments) * 255}, ${cos(TAO * Date.now() / 1000) / 2 * 255}, ${cos(TAO * index / numSegments) * 255}, 0.8)`;
     circle({ x, y, radius });
   });
-  if (drawing)
-    circle({ x: mouseX, y: mouseY, radius: 5 });
   requestAnimationFrame(draw);
 };
-var { PI, cos, sin, sqrt, atan2 } = Math;
+var { PI, cos, sin, sqrt, atan2, abs } = Math;
+var TAO = PI * 2;
+var lerp = (v0, v1, t) => v0 + t * (v1 - v0);
+var normalizeAngle = (angle) => (angle + TAO) % TAO;
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
 var d;
@@ -61,7 +103,8 @@ var numSegments;
 var segments;
 var mouseX;
 var mouseY;
-var drawing = false;
+var controllingWithMouse = false;
+var head;
 canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
   mouseX = clientX;
   mouseY = clientY;
@@ -72,14 +115,19 @@ canvas.addEventListener("touchmove", ({ touches }) => {
   mouseY = clientY;
 });
 canvas.addEventListener("mouseup", ({ clientX, clientY }) => {
-  drawing = false;
+  controllingWithMouse = !controllingWithMouse;
 });
 canvas.addEventListener("mousedown", ({ clientX, clientY }) => {
-  draw();
+});
+document.body.addEventListener("keydown", (event) => {
+  const { key } = event;
+  if (key == "r") {
+    c.fillStyle = "#282828";
+    c.fillRect(0, 0, d.width, d.height);
+  }
 });
 window.addEventListener("resize", () => {
   setCanvasDimensions();
-  draw();
 });
 setCanvasDimensions();
 draw();
