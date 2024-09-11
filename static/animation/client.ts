a8a4215e2242632c24ca66759c7ecacbe783bffa
Human <reedwith2es@gmail.com>
2024-09-10 23:39:14 -0700

    Snake skin multicolored

--- static/animation/client.ts
+++ static/animation/client.ts
@@ -1,10 +1,17 @@
-const { PI, cos, sin, sqrt, atan2, abs } = Math;
+const SHOW_HEAD = false;
+const SHOW_SEGMENTS = false;
+const SHOW_SKIN_POINTS = false;
+const { PI, cos, sin, sqrt, atan2, abs, min, floor } = Math;
 const TAO = PI * 2;
 const PHI = 1.618033988749895;
-const lerp = (v0: number, v1: number, t: number) =>
-    // a * t + (b - a) * t
-    v0 + t * (v1 - v0);
+const lerp = (v0: number, v1: number, t: number) => v0 + t * (v1 - v0);
 const normalizeAngle = (angle: number) => (angle + TAO) % TAO;
+// From https://stackoverflow.com/a/14498790
+const circularLerp = (v0: number, v1: number, t: number) => {
+    const shortestAngle = ((v1 - v0 + PI) % TAO) - PI;
+    // TODO: This still doesn't quite work how I want it to.
+    return lerp(v0, v0 + shortestAngle, t);
+};
 
 // The unit circle starts pointing directly east, and sweeps south then west
 // because Y grows downwards.
@@ -67,24 +74,25 @@ let head: Circle & { direction: number };
 function setup() {
     c.fillStyle = "#282828"; // TODO Get calculated body font color
     c.fillRect(0, 0, d.width, d.height);
-    numSegments = 180;
+    numSegments = 80;
     const radius = d.width / numSegments / 1 - 1;
     segments = Array(numSegments)
         .fill(null)
         .map((_, i) => ({
-            x: (d.width / numSegments) * i + radius + 1,
+            x: 40 + (d.width / numSegments) * i + radius + 1,
             y: d.center.y,
             radius,
         }));
     head = {
         x: segments[0].x,
         y: segments[0].y,
-        radius: radius * 4,
-        direction: 0,
+        radius: radius * PHI,
+        direction: TAO / -8,
     };
 }
 function draw() {
-    c.fillStyle = "rgba(100, 100, 100, 0.001)"; // TODO Get calculated body font color
+    c.fillStyle = "rgba(0, 0, 0, 0.001)"; // TODO Get calculated body font color
+    // c.fillStyle = "#282828"; // TODO Get calculated body font color
     c.fillRect(0, 0, d.width, d.height);
     c.strokeStyle = "white"; // TODO Get calculated body font color
     c.fillStyle = "white"; // TODO Get calculated body font color
@@ -94,16 +102,19 @@ function draw() {
     let nextDirection = head.direction;
     // The head follows the mouse
     if (controllingWithMouse) {
+        head.x = mouseX ?? head.x;
+        head.y = mouseY ?? head.y;
         // Adjust head direction based on movement (none if no movement)
         if (
             abs(head.x - prevHead.x) > 0.0001 &&
             abs(head.y - prevHead.y) > 0.0001
         ) {
             nextDirection = atan2(head.y - prevHead.y, head.x - prevHead.x);
+            head.direction = circularLerp(head.direction, nextDirection, 0.6);
         }
     } else {
-        const wayOut = -50;
-        const margin = -10;
+        const wayOut = -80;
+        const margin = 40;
         // Way out of bounds?
         if (
             d.width - head.x < wayOut ||
@@ -115,8 +126,6 @@ function draw() {
             head.y = d.center.y;
             nextDirection = Math.random() * TAO;
             head.direction = nextDirection;
-            // head.direction = 0;
-            // nextDirection = 0;
         }
         // normal Out of bounds?
         else if (
@@ -128,19 +137,9 @@ function draw() {
             // Turn towards the center
             nextDirection = atan2(d.center.y - head.y, d.center.x - head.x);
         }
-    }
 
-    head.direction =
-        lerp(
-            normalizeAngle(head.direction) + TAO,
-            normalizeAngle(nextDirection) + TAO,
-            0.1,
-        ) % TAO;
+        head.direction = circularLerp(head.direction, nextDirection, 0.02);
 
-    if (controllingWithMouse) {
-        head.x = mouseX ?? head.x;
-        head.y = mouseY ?? head.y;
-    } else {
         const speed = 8;
         // Advance
         head.x += cos(head.direction) * speed;
@@ -161,36 +160,149 @@ function draw() {
         const other = last.y - current.y;
         const hypoteneuse = sqrt(adjacent ** 2 + other ** 2);
         const angle = atan2(other, adjacent);
-        const moveDistance =
-            hypoteneuse - (last.radius + current.radius) * 0.38;
+        const moveDistance = hypoteneuse - (last.radius + current.radius) * 0.3;
 
         current.x += cos(angle) * moveDistance;
         current.y += sin(angle) * moveDistance;
     }
-    let skinPoints: Array<Point>;
-    for (let i = 1; i < segments.length; i++) {
-        let last = segments[i - 1];
-        let current = segments[i];
+    let skinPoints: Array<Point> = [];
+
+    // Points on the head, from left cheek to right before nose
+    const directionToHead = atan2(head.y - first.y, head.x - first.x);
+    const increment = TAO / 16;
+    const cheekCount = 5;
+    for (let i = 0; i < cheekCount; i++) {
+        const angle = directionToHead + increment * (i - cheekCount);
+        skinPoints.push({
+            x: first.x + cos(angle) * first.radius,
+            y: first.y + sin(angle) * first.radius,
+        });
     }
 
-    // c.fillStyle = "rgba(255, 255, 255, 0.2)";
-    // circle({ ...head });
-    // c.strokeStyle = "red";
-    // c.beginPath();
-    // c.moveTo(head.x, head.y);
-    // c.lineTo(
-    //     head.x + cos(head.direction) * PHI * head.radius,
-    //     head.y + sin(head.direction) * PHI * head.radius,
-    // );
-    // c.stroke();
-    segments.forEach(({ x, y, radius }, index) => {
-        // if (index % 2 === 1) return;
-
-        c.fillStyle = `rgba(${255 - cos((TAO * index) / numSegments) * 255}, ${
-            (cos((TAO * Date.now()) / 1000) / 2) * 255
-        }, ${cos((TAO * index) / numSegments) * 255}, 0.8)`; // TODO Get calculated body font color
-        circle({ x, y, radius });
+    // The nose
+    skinPoints.push({
+        x: first.x + cos(directionToHead) * first.radius * PHI,
+        y: first.y + sin(directionToHead) * first.radius * PHI,
     });
+
+    // Now down to the right cheek
+    for (let i = 1; i <= cheekCount; i++) {
+        const angle = directionToHead + increment * i;
+        skinPoints.push({
+            x: first.x + cos(angle) * first.radius,
+            y: first.y + sin(angle) * first.radius,
+        });
+    }
+
+    for (let i = 1; i < segments.length - 1; i++) {
+        let last = segments[i - 1];
+        let current = segments[i];
+        const direction = atan2(last.y - current.y, last.x - current.x);
+
+        const increment = TAO / 8;
+        // At the end we push the right side
+        skinPoints.push({
+            x: current.x + cos(direction + increment * 1) * current.radius,
+            y: current.y + sin(direction + increment * 1) * current.radius,
+        });
+        skinPoints.push({
+            x: current.x + cos(direction + increment * 2) * current.radius,
+            y: current.y + sin(direction + increment * 2) * current.radius,
+        });
+        skinPoints.push({
+            x: current.x + cos(direction + increment * 3) * current.radius,
+            y: current.y + sin(direction + increment * 3) * current.radius,
+        });
+
+        // At the beginning we unshift the left side
+        skinPoints.unshift({
+            x: current.x + cos(direction - increment * 1) * current.radius,
+            y: current.y + sin(direction - increment * 1) * current.radius,
+        });
+        skinPoints.unshift({
+            x: current.x + cos(direction - increment * 2) * current.radius,
+            y: current.y + sin(direction - increment * 2) * current.radius,
+        });
+        skinPoints.unshift({
+            x: current.x + cos(direction - increment * 3) * current.radius,
+            y: current.y + sin(direction - increment * 3) * current.radius,
+        });
+    }
+
+    let secondToLast = segments.at(-2)!;
+    let last = segments.at(-1)!;
+    const lastDirection = atan2(
+        secondToLast.y - last.y,
+        secondToLast.x - last.x,
+    );
+    for (
+        let i = lastDirection + PI - 5 * increment;
+        i < lastDirection + PI + 6 * increment;
+        i += increment
+    ) {
+        skinPoints.push({
+            x: last.x + cos(i) * last.radius,
+            y: last.y + sin(i) * last.radius,
+        });
+    }
+
+    // An indicator as to the head, for development purposes.
+    if (SHOW_HEAD) {
+        c.fillStyle = "rgba(255, 255, 255, 0.2)";
+        circle({ ...head });
+        c.strokeStyle = "tomato";
+        c.lineWidth = 3;
+        c.lineCap = "round";
+        c.beginPath();
+        c.moveTo(head.x, head.y);
+        c.lineTo(
+            head.x + cos(head.direction) * PHI * head.radius,
+            head.y + sin(head.direction) * PHI * head.radius,
+        );
+        c.stroke();
+    }
+
+    // The segments
+    if (SHOW_SEGMENTS) {
+        segments.forEach(({ x, y, radius }, index) => {
+            c.fillStyle = `rgba(170, 156, 40, 0.8)`; // TODO Get calculated body font color
+            circle({ x, y, radius });
+        });
+    }
+
+    // The skin
+    if (SHOW_SKIN_POINTS) {
+        skinPoints.forEach(({ x, y }) => {
+            c.fillStyle = `rgba(40, 156, 170, 0.8)`; // TODO Get calculated body font color
+            circle({ x, y, radius: 5 });
+        });
+    }
+
+    const now = Date.now();
+    for (let index = 1; index < skinPoints.length; index++) {
+        const last = skinPoints[index - 1];
+        const current = skinPoints[index];
+        const i = index / skinPoints.length;
+        const t = (cos((TAO * now) / 2000) + 1) / 2;
+
+        c.beginPath();
+        c.moveTo(last.x, last.y);
+        c.strokeStyle = `rgba(${floor(i * 255)}, ${floor(t * 255)}, ${floor(
+            floor(255 - i * 255),
+        )}, 1)`;
+        c.lineTo(current.x, current.y);
+        c.stroke();
+    }
+
+    {
+        const last = skinPoints.at(-1)!;
+        const first = skinPoints.at(0)!;
+        c.beginPath();
+        c.moveTo(last.x, last.y);
+        c.lineTo(first.x, first.y);
+        c.stroke();
+        c.closePath();
+    }
 
     requestAnimationFrame(draw);
 }
--- static/animation/out/client.js
+++ static/animation/out/client.js
@@ -21,34 +21,37 @@ var circle = function({ x, y, radius }) {
 var setup = function() {
   c.fillStyle = "#282828";
   c.fillRect(0, 0, d.width, d.height);
-  numSegments = 180;
+  numSegments = 80;
   const radius = d.width / numSegments / 1 - 1;
   segments = Array(numSegments).fill(null).map((_, i) => ({
-    x: d.width / numSegments * i + radius + 1,
+    x: 40 + d.width / numSegments * i + radius + 1,
     y: d.center.y,
     radius
   }));
   head = {
     x: segments[0].x,
     y: segments[0].y,
-    radius: radius * 4,
-    direction: 0
+    radius: radius * PHI,
+    direction: TAO / -8
   };
 };
 var draw = function() {
-  c.fillStyle = "rgba(100, 100, 100, 0.001)";
+  c.fillStyle = "rgba(0, 0, 0, 0.001)";
   c.fillRect(0, 0, d.width, d.height);
   c.strokeStyle = "white";
   c.fillStyle = "white";
   const prevHead = { ...head };
   let nextDirection = head.direction;
   if (controllingWithMouse) {
+    head.x = mouseX ?? head.x;
+    head.y = mouseY ?? head.y;
     if (abs(head.x - prevHead.x) > 0.0001 && abs(head.y - prevHead.y) > 0.0001) {
       nextDirection = atan2(head.y - prevHead.y, head.x - prevHead.x);
+      head.direction = circularLerp(head.direction, nextDirection, 0.6);
     }
   } else {
-    const wayOut = -50;
-    const margin = -10;
+    const wayOut = -80;
+    const margin = 40;
     if (d.width - head.x < wayOut || d.height - head.y < wayOut || head.x < wayOut || head.y < wayOut) {
       head.x = d.center.x;
       head.y = d.center.y;
@@ -57,12 +60,7 @@ var draw = function() {
     } else if (d.width - head.x < margin || d.height - head.y < margin || head.x < margin || head.y < margin) {
       nextDirection = atan2(d.center.y - head.y, d.center.x - head.x);
     }
-  }
-  head.direction = lerp(normalizeAngle(head.direction) + TAO, normalizeAngle(nextDirection) + TAO, 0.1) % TAO;
-  if (controllingWithMouse) {
-    head.x = mouseX ?? head.x;
-    head.y = mouseY ?? head.y;
-  } else {
+    head.direction = circularLerp(head.direction, nextDirection, 0.02);
     const speed = 8;
     head.x += cos(head.direction) * speed;
     head.y += sin(head.direction) * speed;
@@ -71,31 +69,134 @@ var draw = function() {
   first.x = lerp(first.x, head.x, 0.07);
   first.y = lerp(first.y, head.y, 0.07);
   for (let i = 1;i < segments.length; i++) {
-    let last = segments[i - 1];
+    let last2 = segments[i - 1];
     let current = segments[i];
-    const adjacent = last.x - current.x;
-    const other = last.y - current.y;
+    const adjacent = last2.x - current.x;
+    const other = last2.y - current.y;
     const hypoteneuse = sqrt(adjacent ** 2 + other ** 2);
     const angle = atan2(other, adjacent);
-    const moveDistance = hypoteneuse - (last.radius + current.radius) * 0.38;
+    const moveDistance = hypoteneuse - (last2.radius + current.radius) * 0.3;
     current.x += cos(angle) * moveDistance;
     current.y += sin(angle) * moveDistance;
   }
-  let skinPoints;
-  for (let i = 1;i < segments.length; i++) {
-    let last = segments[i - 1];
-    let current = segments[i];
+  let skinPoints = [];
+  const directionToHead = atan2(head.y - first.y, head.x - first.x);
+  const increment = TAO / 16;
+  const cheekCount = 5;
+  for (let i = 0;i < cheekCount; i++) {
+    const angle = directionToHead + increment * (i - cheekCount);
+    skinPoints.push({
+      x: first.x + cos(angle) * first.radius,
+      y: first.y + sin(angle) * first.radius
+    });
   }
-  segments.forEach(({ x, y, radius }, index) => {
-    c.fillStyle = `rgba(${255 - cos(TAO * index / numSegments) * 255}, ${cos(TAO * Date.now() / 1000) / 2 * 255}, ${cos(TAO * index / numSegments) * 255}, 0.8)`;
-    circle({ x, y, radius });
+  skinPoints.push({
+    x: first.x + cos(directionToHead) * first.radius * PHI,
+    y: first.y + sin(directionToHead) * first.radius * PHI
   });
+  for (let i = 1;i <= cheekCount; i++) {
+    const angle = directionToHead + increment * i;
+    skinPoints.push({
+      x: first.x + cos(angle) * first.radius,
+      y: first.y + sin(angle) * first.radius
+    });
+  }
+  for (let i = 1;i < segments.length - 1; i++) {
+    let last2 = segments[i - 1];
+    let current = segments[i];
+    const direction = atan2(last2.y - current.y, last2.x - current.x);
+    const increment2 = TAO / 8;
+    skinPoints.push({
+      x: current.x + cos(direction + increment2 * 1) * current.radius,
+      y: current.y + sin(direction + increment2 * 1) * current.radius
+    });
+    skinPoints.push({
+      x: current.x + cos(direction + increment2 * 2) * current.radius,
+      y: current.y + sin(direction + increment2 * 2) * current.radius
+    });
+    skinPoints.push({
+      x: current.x + cos(direction + increment2 * 3) * current.radius,
+      y: current.y + sin(direction + increment2 * 3) * current.radius
+    });
+    skinPoints.unshift({
+      x: current.x + cos(direction - increment2 * 1) * current.radius,
+      y: current.y + sin(direction - increment2 * 1) * current.radius
+    });
+    skinPoints.unshift({
+      x: current.x + cos(direction - increment2 * 2) * current.radius,
+      y: current.y + sin(direction - increment2 * 2) * current.radius
+    });
+    skinPoints.unshift({
+      x: current.x + cos(direction - increment2 * 3) * current.radius,
+      y: current.y + sin(direction - increment2 * 3) * current.radius
+    });
+  }
+  let secondToLast = segments.at(-2);
+  let last = segments.at(-1);
+  const lastDirection = atan2(secondToLast.y - last.y, secondToLast.x - last.x);
+  for (let i = lastDirection + PI - 5 * increment;i < lastDirection + PI + 6 * increment; i += increment) {
+    skinPoints.push({
+      x: last.x + cos(i) * last.radius,
+      y: last.y + sin(i) * last.radius
+    });
+  }
+  if (SHOW_HEAD) {
+    c.fillStyle = "rgba(255, 255, 255, 0.2)";
+    circle({ ...head });
+    c.strokeStyle = "tomato";
+    c.lineWidth = 3;
+    c.lineCap = "round";
+    c.beginPath();
+    c.moveTo(head.x, head.y);
+    c.lineTo(head.x + cos(head.direction) * PHI * head.radius, head.y + sin(head.direction) * PHI * head.radius);
+    c.stroke();
+  }
+  if (SHOW_SEGMENTS) {
+    segments.forEach(({ x, y, radius }, index) => {
+      c.fillStyle = `rgba(170, 156, 40, 0.8)`;
+      circle({ x, y, radius });
+    });
+  }
+  if (SHOW_SKIN_POINTS) {
+    skinPoints.forEach(({ x, y }) => {
+      c.fillStyle = `rgba(40, 156, 170, 0.8)`;
+      circle({ x, y, radius: 5 });
+    });
+  }
+  const now = Date.now();
+  for (let index = 1;index < skinPoints.length; index++) {
+    const last2 = skinPoints[index - 1];
+    const current = skinPoints[index];
+    const i = index / skinPoints.length;
+    const t = (cos(TAO * now / 2000) + 1) / 2;
+    c.beginPath();
+    c.moveTo(last2.x, last2.y);
+    c.strokeStyle = `rgba(${floor(i * 255)}, ${floor(t * 255)}, ${floor(floor(255 - i * 255))}, 1)`;
+    c.lineTo(current.x, current.y);
+    c.stroke();
+  }
+  {
+    const last2 = skinPoints.at(-1);
+    const first2 = skinPoints.at(0);
+    c.beginPath();
+    c.moveTo(last2.x, last2.y);
+    c.lineTo(first2.x, first2.y);
+    c.stroke();
+    c.closePath();
+  }
   requestAnimationFrame(draw);
 };
-var { PI, cos, sin, sqrt, atan2, abs } = Math;
+var SHOW_HEAD = false;
+var SHOW_SEGMENTS = false;
+var SHOW_SKIN_POINTS = false;
+var { PI, cos, sin, sqrt, atan2, abs, min, floor } = Math;
 var TAO = PI * 2;
+var PHI = 1.618033988749895;
 var lerp = (v0, v1, t) => v0 + t * (v1 - v0);
-var normalizeAngle = (angle) => (angle + TAO) % TAO;
+var circularLerp = (v0, v1, t) => {
+  const shortestAngle = (v1 - v0 + PI) % TAO - PI;
+  return lerp(v0, v0 + shortestAngle, t);
+};
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
 var d;
