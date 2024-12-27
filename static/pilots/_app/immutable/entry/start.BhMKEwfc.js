8e9390b30c25d6270181e711e788e3a5c6dd1114
Human <reedwith2es@gmail.com>
2024-12-27 01:33:49 -0800

    Uploading game hash 698e91e-dirty

---
+++ static/pilots/_app/immutable/entry/start.BhMKEwfc.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bk2zn1tT.js";export{t as start};
---
+++ static/pilots/_app/immutable/nodes/3.CNroniao.js
@@ -0,0 +1,18 @@
+import{a,t}from"../chunks/disclose-version.Chs3MZKY.js";import"../chunks/legacy.DUy_6n_X.js";import{e as n}from"../chunks/runtime.CSRt9162.js";var r=t(`<h1>Design Notes</h1> <p><a href="/">Back to game title screen</a></p> <h2>History</h2> <p>This game is a confluence of disparate interests of mine. Maybe all those
+interests were inevitably related, or maybe this game is my attempt to fit all
+my favorite tchotchkes in some wrong-sized luggage.</p> <p>An image which has bounced around in my head for a long time is a mad
+scientist’s wall filled with an uneven grid of gadgets, panels, latches, knobs,
+switches, etc. An unending plane complications. I wonder if I’ve ever seen it or
+if I’ve pieced together the image in my mind’s eye from tropes. See Dexter’s Lab,
+Dr. Frankenstein, Dr. Strangelove.</p> <p>I wrote a short story about a pilot who gets drunk, hops in his training mech,
+destroys his commanding officer’s building, and watches the war playout from a
+prison cell. I wrote another story of a pilot who wakes up in a bed of
+jelly-like tub of micro-organisms. I wanted to explore “different mechs
+for different folks,” as opposed to a world where the big robots are
+one-type-of-thing. See <a href="https://matrix.fandom.com/wiki/Armored_Personnel_Unit" rel="nofollow">The Matrix
+(APUs)</a>, Gurran Lagann,
+Neon Genesis Evangelion, Macross, Gundam, Pacific Rim.</p> <p>For visual aesthetics, I searched Pinterest for “anime UI”, “fantasy UI”. I
+watched a YouTube video <a href="https://www.youtube.com/watch?v=brpKGTnkVdI" rel="nofollow">“Evangelion UI, Sound Designed” by Yuta Endo 遠藤ユウ
+タ</a>. “Anime UI” search on CodePen
+yielded <a href="https://codepen.io/alliwalk/pen/aboyKXz" rel="nofollow">a fun example</a>. “Futuristic”
+was a better search there, yielding more good stuff: <a href="https://codepen.io/nw/pen/QWgpgQ" rel="nofollow">[1]</a> <a href="https://codepen.io/thpoi/pen/GYBZar" rel="nofollow">[2]</a> <a href="https://codepen.io/LukyVj/pen/xxyEYMJ" rel="nofollow">[3]</a> <a href="https://codepen.io/puritanner/pen/BKzqMW" rel="nofollow">[4]</a></p> <p><a href="/">Back to game title screen</a></p>`,1);function p(e){var o=r();n(14),a(e,o)}export{p as component};
